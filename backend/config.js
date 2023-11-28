104003f1cba4e154060f18595b8b1539d8ea4609
Sahilrawatgithub <sahil1229.be21@chitkara.edu.in>
2023-11-28 21:25:02 +0530

    init repo

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://sahilrawat12741:V6lHguyo3n9y7nxz@cluster0.mins2uh.mongodb.net/?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
